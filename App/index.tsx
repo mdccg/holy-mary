847497507e1aa02c8a102ba8dc88cc05761bd53d
Matheus Daniel Cristal Comparotto Gomes <comparotto.js@gmail.com>
2023-05-28 19:54:35 -0400

    Fixed fonts issue

--- App/index.tsx
+++ App/index.tsx
@@ -1,16 +1,41 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
+import { useCallback } from 'react';
 import 'react-native-gesture-handler';
-import FontStyles from './../assets/fonts';
 import StatusBarHeight from './../src/components/StatusBarHeight';
 import Routes from './../src/routes';
 import { AppWrapper } from './styles';
 
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./../assets/fonts/Inter/Inter-Black.ttf'),
+    'Inter-Bold': require('./../assets/fonts/Inter/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('./../assets/fonts/Inter/Inter-ExtraBold.ttf'),
+    'Inter-ExtraLight': require('./../assets/fonts/Inter/Inter-ExtraLight.ttf'),
+    'Inter-Light': require('./../assets/fonts/Inter/Inter-Light.ttf'),
+    'Inter-Medium': require('./../assets/fonts/Inter/Inter-Medium.ttf'),
+    'Inter-Regular': require('./../assets/fonts/Inter/Inter-Regular.ttf'),
+    'Inter-SemiBold': require('./../assets/fonts/Inter/Inter-SemiBold.ttf'),
+    'Inter-Thin': require('./../assets/fonts/Inter/Inter-Thin.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return (
-    <AppWrapper>
-      <StatusBar style="auto" translucent />
+    <AppWrapper onLayout={onLayoutRootView}>
+      <StatusBar style="auto" />
       <StatusBarHeight />
-      <FontStyles />
       <Routes />
     </AppWrapper>
   );
--- src/stylesheets/theme.ts
+++ src/stylesheets/theme.ts
@@ -1,3 +1,17 @@
-const theme = {};
+const theme = {
+  fonts: {
+    inter: {
+      black: 'Inter-Black',
+      bold: 'Inter-Bold',
+      extraBold: 'Inter-ExtraBold',
+      extraLight: 'Inter-ExtraLight',
+      light: 'Inter-Light',
+      medium: 'Inter-Medium',
+      regular: 'Inter-Regular',
+      semiBold: 'Inter-SemiBold',
+      thin: 'Inter-Thin',
+    },
+  }
+};
 
 export default theme;
