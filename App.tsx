e60bab9cf9f93291933ffc7b2b924e33eaf263bb
Matheus Daniel Cristal Comparotto Gomes <comparotto.js@gmail.com>
2023-05-28 23:57:54 -0400

    Removed App/ dir to be compatible with Snack

---
+++ App.tsx
@@ -0,0 +1,42 @@
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useCallback } from 'react';
+import 'react-native-gesture-handler';
+import { ThemeProvider } from 'styled-components/native';
+import Routes from './src/routes';
+import theme from './src/stylesheets/theme';
+import { AppWrapper } from './AppStyles';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    'Inter-Black': require('./assets/fonts/Inter/Inter-Black.ttf'),
+    'Inter-Bold': require('./assets/fonts/Inter/Inter-Bold.ttf'),
+    'Inter-ExtraBold': require('./assets/fonts/Inter/Inter-ExtraBold.ttf'),
+    'Inter-ExtraLight': require('./assets/fonts/Inter/Inter-ExtraLight.ttf'),
+    'Inter-Light': require('./assets/fonts/Inter/Inter-Light.ttf'),
+    'Inter-Medium': require('./assets/fonts/Inter/Inter-Medium.ttf'),
+    'Inter-Regular': require('./assets/fonts/Inter/Inter-Regular.ttf'),
+    'Inter-SemiBold': require('./assets/fonts/Inter/Inter-SemiBold.ttf'),
+    'Inter-Thin': require('./assets/fonts/Inter/Inter-Thin.ttf')
+  });
+
+  const onLayoutRootView = useCallback(async () => {
+    if (fontsLoaded) {
+      await SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+  
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return (
+    <AppWrapper onLayout={onLayoutRootView}>
+      <ThemeProvider theme={theme}>
+        <Routes />
+      </ThemeProvider>
+    </AppWrapper>
+  );
+}
