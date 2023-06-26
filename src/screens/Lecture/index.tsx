8f6e95189d659771ad6799ea4936fa02cafd8f78
Nathan Alves <99689990+NathanAlv@users.noreply.github.com>
2023-06-26 04:38:14 -0400

    Pacht: texto a mostra, falta editar

--- src/screens/Lecture/index.tsx
+++ src/screens/Lecture/index.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from 'react';
 import CustomStatusBar from '../../components/CustomStatusBar';
 import BibleService from '../../services/BibleService';
 import ScreenProps from '../../types/ScreenProps';
-import { LectureWrapper } from './styles';
+import { LectureWrapper, Verses } from './styles';
+import VerseDTO from '../../data-transports/VerseDTO';
+import { Text } from 'react-native';
 
 type LectureProps = ScreenProps<'Lecture'>;
 
@@ -18,12 +20,24 @@ const Lecture = ({
     }
   }
 }: LectureProps) => {
+  console.log( book.abbrev.en, chapter)
   const [bibleService] = useState<BibleService>(new BibleService());
 
   const getChapter = async () => {
     const verses = await bibleService.retrieveVersesByChapter('nvi', book.abbrev.en, chapter);
-    console.log(verses);
+    console.log(verses); 
+   
+    setVerses(verses);
+
+    
   }
+
+  const [verses, setVerses] = useState<VerseDTO[]>([])
+  
+
+  
+ 
+  
 
   useEffect(() => {
     getChapter();
@@ -34,10 +48,10 @@ const Lecture = ({
       <CustomStatusBar />
 
       <LectureWrapper>
-        {/* Todo o conteúdo da tela vem aqui */}
+         {verses.map((verse) => <Verses>{verse.text}</Verses>)} 
       </LectureWrapper>
     </>
   );
 }
 
-export default Lecture;
+export default Lecture
--- src/screens/Lecture/styles.ts
+++ src/screens/Lecture/styles.ts
@@ -1,3 +1,9 @@
 import styled from 'styled-components/native';
 
-export const LectureWrapper = styled.ScrollView``;
+export const LectureWrapper = styled.ScrollView`
+ 
+`;
+
+export const Verses = styled.ScrollView`
+   padding-left: 10000px;
+`;
